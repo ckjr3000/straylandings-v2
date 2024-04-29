55d859bbbe2627e3fc27bceb92334423e02ac943
ckjr3000 <charlotte.kj.roe@gmail.com>
2024-04-29 15:08:35 +0100

    Adds and configures Decap CMS

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,20 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/img");
     eleventyConfig.addPassthroughCopy("assets/css");
+    eleventyConfig.addPassthroughCopy("admin");
+
+      // Format date 
+      eleventyConfig.addFilter("postDate", (dateObj) => {
+          return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+      })
+
+      return {
+        dir: {
+            input: "src",
+            output: "_site"
+        }
+    }
   };
 
