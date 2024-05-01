3c52c3d4580b8255516e39e2928775e0653a3953
ckjr3000 <charlotte.kj.roe@gmail.com>
2024-05-01 11:21:36 +0100

    Configures homepage to pull through the correct articles
    latest for latest section, latest 20 from all categories for main section, exludes admin home and home from collections.all

--- .eleventy.js
+++ .eleventy.js
@@ -4,17 +4,17 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/img");
     eleventyConfig.addPassthroughCopy("src/assets/css");
     eleventyConfig.addPassthroughCopy("src/admin/config.yml");
+    
+    // Format date 
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    })
 
-      // Format date 
-      eleventyConfig.addFilter("postDate", (dateObj) => {
-          return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-      })
-
-      return {
-        dir: {
-            input: "src",
-            output: "_site"
-        }
+    return {
+      dir: {
+          input: "src",
+          output: "_site"
+      }
     }
   };
 
