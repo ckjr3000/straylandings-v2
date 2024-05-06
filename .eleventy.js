afd686d3d891727f9581677ce7d71cc5211c2357
ckjr3000 <charlotte.kj.roe@gmail.com>
2024-05-06 16:41:40 +0100

    Brings fonts folder across from old repo
    Changes passthrough for css, img and fonts to just passthrough assets folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,7 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/assets/img");
-    eleventyConfig.addPassthroughCopy("src/assets/css");
+    eleventyConfig.addPassthroughCopy("src/assets");
     eleventyConfig.addPassthroughCopy("src/admin/config.yml");
     
     // Format date 
