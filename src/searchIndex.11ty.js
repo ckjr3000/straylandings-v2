6be62997f538eca1586c3eae778133d553233306
ckjr3000 <charlotte.kj.roe@gmail.com>
2024-05-20 11:25:50 +0100

    Fixes errors fetching seearch results
    path to searchIndex.json created, div with id of 'search-results' added to search page template

--- src/searchIndex.11ty.js
+++ src/searchIndex.11ty.js
@@ -3,21 +3,15 @@ module.exports.data = {
     eleventyExcludeFromCollections: true,
   };
   
-  module.exports.render = (data) => {
+    module.exports.render = (data) => {
     const items = data.collections.all
-      .filter((item) => !item.eleventyExcludeFromCollections)
-      .map((item) => ({
-        title: item.data.title,
-        url: item.url,
-        date: item.date,
-        content: item.templateContent,
-      }));
-  
-    // Wrap the items array in an object with a key, for example, `items`
-    const output = {
-      items: items,
-    };
-  
-    return JSON.stringify(output, null, 2);
-  };
-  
+        .filter((item) => !item.eleventyExcludeFromCollections)
+        .map((item) => ({
+            title: item.data.title,
+            url: item.url,
+            date: item.date,
+            content: item.templateContent,
+        }));
+
+    return JSON.stringify(items, null, 2);
+};
